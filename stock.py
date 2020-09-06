90391c691c846e8cf90ac5cbbceb07be07c769a2
suryadevara venkateswararao <suryadevarav@vmware.com>
2020-09-06 20:32:30 +0530

    check the diff of high to open market price for last specific number of days

---
+++ stock.py
@@ -0,0 +1,74 @@
+# SRC https://thispointer.com/pandas-count-rows-in-a-dataframe-all-or-those-only-that-satisfy-a-condition/#:~:text=apply()%20we%20can%20apply,dataframe%20that%20satisfies%20the%20condition.
+
+# Step1: Install Pandas - pip instal pandas
+
+# Step2: Download the CSV file from the website
+
+# Step3: Execute the below code
+
+# Usage: python2 stock.py --list "LUPIN" 10 30
+# LUPIN - exact company code
+# 10 - diff b/w high to open price on a particular day
+# 30 - analysis on last specific number of days
+
+import argparse
+import requests
+import os
+import pandas as pd
+
+class Company:
+
+	def __init__(self, open_high_diff, last_no_of_days):
+
+		self.open_high_diff = open_high_diff
+		self.last_no_of_days = last_no_of_days
+
+	def get_open_high_diff(self, company, type='NS'):
+
+		url = self.get_url(company, type)
+		r = requests.get(url, allow_redirects=True)
+
+		home = os.path.expanduser("~")
+		file_path = os.path.join(home, "Downloads/")
+
+		open(file_path+company+'.'+type+'.csv', 'wb').write(r.content)
+
+		#read the CSV file downloaded
+		df_read = pd.read_csv(file_path+company+"."+type+".csv")
+
+		col_list = ["Open", "High"]
+		StockDfObj = pd.DataFrame(df_read, columns=col_list)
+
+		#applying filter based on the high and open value difference
+		seriesObj = StockDfObj.apply(lambda x: True if x['High'] - x['Open'] > int(self.open_high_diff) else False , axis=1)
+		seriesObj = seriesObj.head(int(self.last_no_of_days))
+
+		numOfRows = len(seriesObj[seriesObj == True].index)
+
+		#returning the number of rows which has high to open value > specified value
+		return numOfRows
+
+	def get_url(self, company, type):
+		try:
+			return "https://query1.finance.yahoo.com/v7/finance/download/"+company+"."+type+"?period1=1567765404&period2=1599387804&interval=1d&events=history"
+		except Exception:
+			print("{0} data not appicable for this company".format(type))
+			pass
+
+
+if __name__=="__main__":
+
+	import argparse
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--list", nargs="+", default=["HDFC", 3, 15])
+	args = parser.parse_args()
+	print("See below {0} company with {1} as high to open diff in {2} days".format(args.list[0], args.list[1], args.list[2]))
+
+	c1 = Company(args.list[1], args.list[2])
+	result = c1.get_open_high_diff(args.list[0])
+	print("NSE high to open diff is {0} for {1} in last {2} days".format(args.list[1], result, args.list[2]))
+
+	c2 = Company(args.list[1], args.list[2])
+	result = c2.get_open_high_diff(args.list[0], type='BO')
+	print("BSE high to open diff is {0} for {1} in last {2} days".format(args.list[1], result, args.list[2]))
